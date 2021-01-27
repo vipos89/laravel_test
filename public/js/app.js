ce178fc44b90b8582f08757a5f06cb233a6bbf5f
vipos89 <webmaster.kobrin@gmail.com>
2021-01-27 20:13:35 +0300

    init

--- public/js/app.js
+++ public/js/app.js
@@ -37400,8 +37400,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/code/mapper.loc/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/code/mapper.loc/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\OSPanel\domains\mapper.loc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\OSPanel\domains\mapper.loc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
